696722d11c042b3cddf8e6ea06a5c396805b8eb6
SamHwang1990 <samhwang1990@gmail.com>
2016-08-10 23:07:22 +0800

    complete the 19th chapter practice: 1. come some music

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -1,7 +1,12 @@
 package com.bignerdranch.android.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.util.Log;
 
 import java.io.IOException;
@@ -15,16 +20,29 @@ public class BeatBox {
 
     private static final String TAG = "BeatBox";
     private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUNDS = 5;
 
     private AssetManager mAssetManager;
     private List<Sound> mSoundList = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssetManager = context.getAssets();
-        loadSound();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+        } else {
+            SoundPool.Builder soundPoolBuilder = new SoundPool.Builder();
+            soundPoolBuilder.setMaxStreams(MAX_SOUNDS);
+
+            AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
+            attrBuilder.setLegacyStreamType(AudioManager.STREAM_MUSIC);
+
+            mSoundPool = soundPoolBuilder.setAudioAttributes(attrBuilder.build()).build();
+        }
+        loadSounds();
     }
 
-    private void loadSound() {
+    private void loadSounds() {
         String[] soundNames;
         try {
             soundNames = mAssetManager.list(SOUNDS_FOLDER);
@@ -34,6 +52,7 @@ public class BeatBox {
             for (String fileName: soundNames) {
                 String assetPath = SOUNDS_FOLDER + "/" + fileName;
                 Sound sound = new Sound(assetPath);
+                loadSound(sound);
                 mSoundList.add(sound);
             }
         } catch (IOException ioe) {
@@ -41,7 +60,26 @@ public class BeatBox {
         }
     }
 
+    private void loadSound(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssetManager.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
+    }
+
     public List<Sound> listSounds() {
         return mSoundList;
+    }
+
+    public void play(Sound sound) {
+        Integer soundId = sound.getSoundId();
+        if (soundId == null) {
+            return;
+        }
+
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void release() {
+        mSoundPool.release();
     }
 }
